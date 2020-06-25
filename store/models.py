5861014bf5f19471b2b3720a4824aa3523e23612
Denis <denisolyanyuk@gmail.com>
2020-06-25 21:59:29 +0300

    start navbar

--- store/models.py
+++ store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    price = models.PositiveSmallIntegerField()
+    main_image = models.ImageField()
+    title = models.CharField(max_length=50)
+
+
+class ItemImages(models.Model):
+    image = models.ImageField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
